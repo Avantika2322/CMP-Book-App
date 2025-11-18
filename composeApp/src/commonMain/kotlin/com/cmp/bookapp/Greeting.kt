14e5c307ebf79b3ffe335cbdded8c653d8e3f9c1
avantika <avantika.rao@binarysemantics.com>
2025-11-18 14:47:28 +0530

    initial commit

---
+++ composeApp/src/commonMain/kotlin/com/cmp/bookapp/Greeting.kt
@@ -0,0 +1,9 @@
+package com.cmp.bookapp
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/commonTest/kotlin/com/cmp/bookapp/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package com.cmp.bookapp
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
