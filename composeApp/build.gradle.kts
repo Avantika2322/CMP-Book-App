4066d548748f18e9931aa723567bf576330b7a87
avantika <avantika.rao@binarysemantics.com>
2025-11-25 13:39:44 +0530

    initial commit

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -36,6 +36,7 @@ kotlin {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
             implementation(libs.ktor.client.okhttp)
+            implementation(libs.kotlinx.coroutines.android)
         }
 
         commonMain.dependencies {
